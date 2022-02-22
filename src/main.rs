94f75639642d6fc8fe15a0ce783a3a1b742c047c
Hakan BİRİŞ <hakanbiris@icloud.com>
2022-02-23 01:50:45 +0300

    çalışma yapıldı.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
 extern crate postgres;
 
-use postgres::{Client, NoTls};
+use postgres::{Client, Error, NoTls};
+
 fn main() {
-    println!("Hello, world!");
+    let veritabani = db_baglanti_fn();
+
+    match veritabani {
+        Ok(veritabani) => println!("bağlantı sağlandı"),
+        Err(e) => {
+            println!("işlem başarılı değil");
+            println!("{}", e)
+        }
+    }
+
+    println!("hakan");
+
+    //    if veritabani.is_err() {
+    //        println!("hatalı ");
+    //    } else {
+    //        println!("bağlantı sağlandı")
+    //    }
+}
+
+fn db_baglanti_fn() -> Result<Client, Error> {
+    let db_baglanti = Client::connect(
+        "postgresql://hakanbiris:Turgutlu:45@192.168.1.30/soszluk",
+        NoTls,
+    )?;
+
+    Ok(db_baglanti)
 }
