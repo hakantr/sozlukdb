b5e8a56f2c9adf9665d095621d9505e0f6f188d8
Hakan BİRİŞ <hakanbiris@icloud.com>
2022-02-25 03:54:04 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-extern crate postgres;
-
-use postgres::{Client, Error, NoTls};
+use postgres::{Client, Error, Row, NoTls};
 
 fn main() {
-    let veritabani = db_baglanti_fn();
+    let mut veritabani = db_baglanti_fn();
 
     match veritabani {
-        Ok(veritabani) => println!("Bağlantı sağlandı."),
+        Ok(veritabani) => {
+           let mut sorgu = db_sorgusu_fn( veritabani);
+            println!("denden")
+        },
         Err(e) => {
             println!("Bağlantıda hata var!");
             println!("{:?}", e.as_db_error())
@@ -24,9 +25,22 @@ fn main() {
 
 fn db_baglanti_fn() -> Result<Client, Error> {
     let db_baglanti = Client::connect(
-        "postgresql://hakanbiris:Turgutlu:45@192.168.1.30/soz1luk",
+        "postgresql://hakanbiris:Turgutlu:45@192.168.1.30/sozluk",
         NoTls,
     )?;
 
     Ok(db_baglanti)
+
+}
+
+
+
+fn db_sorgusu_fn(mut veritabani: Client) -> Result<Vec<Row>, Error> {
+
+    let sorgu = veritabani.query(
+        "select * from kelimeler limit 25",
+        &[]
+    )?;
+
+    Ok(sorgu)
 }
