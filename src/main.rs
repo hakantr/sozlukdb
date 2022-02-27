eb93ca20701367d3280e6e2c1e0a68d1ba097ef7
Hakan BİRİŞ <hakanbiris@icloud.com>
2022-02-28 02:20:22 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,50 @@ fn main() {
 
     //let mut veritabani = db_baglanti_fn();
 
-    let mut veritabani:Option<Client> = match db_baglanti_fn() {
-        Ok(veritabani) => Some(veritabani),
-        Err(e) => None,
-    };
+    // let mut veritabani:Option<Client> = match db_baglantı_fn(&mut veritabani) {
+    //     Ok(veritabani) => Some(veritabani),
+    //     Err(e) => None,
+    // };
+    let mut veritabani:Option<Client>=None;
     let mut satirlar:Option<Vec<Row>>=None;
 
+    db_baglantı_fn(&mut veritabani);
     match veritabani {
         Some(mut veritabani) => {
             db_sorgusu_fn(&mut veritabani, &mut satirlar);
         },
         None => println!("Bağlantı başarısız!")
     }
-    println!("hakan");
+
+    for i in Some(satirlar) {
+        let soz = i.get(1);
+        println!("{:?}", soz);
+    }
 }
 
-fn db_baglanti_fn() -> Result<Client, Error> {
-    let db_baglanti = Client::connect(
-        "postgresql://hakanbiris:Turgutlu:45@192.168.1.30/sozluk",
-        NoTls,
-    )?;
+fn db_baglantı_fn(veritabani: &mut Option<Client>){
 
-    Ok(db_baglanti)
+    *veritabani = match Client::connect("postgresql://hakanbiris:Turgutlu:45@192.168.1.30/sozluk", NoTls )
+    {
+        Ok(T) => {Some(T)},
+        Err(e) => {None},
+    };
 }
+
+// fn db_baglanti_fn() -> Result<Client, Error> {
+//     let db_baglanti = Client::connect(
+//         "postgresql://hakanbiris:Turgutlu:45@192.168.1.30/sozluk",
+//         NoTls,
+//     )?;
+//
+//     Ok(db_baglanti)
+// }
 
 fn db_sorgusu_fn(veritabani: &mut Client, satirlar: &mut Option<Vec<Row>>){
-    *satirlar = match veritabani.query("select * from kelimeler limit 25",&[]){
+    *satirlar = match veritabani.query("select id, sozcuk, kok from kelimeler limit 25",&[]){
         Ok(T) => {Some(T)},
         Err(e) =>{None},
     };
 }
+
 
